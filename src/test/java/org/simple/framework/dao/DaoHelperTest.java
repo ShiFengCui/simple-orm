d5483cb9aed1e413f3f20a53fd84e5d2fa320d37
cuishifeng <cuishifeng0207@163.com>
2022-06-07 17:07:07 +0800

    add testCreateInstance

---
+++ src/test/java/org/simple/framework/dao/DaoHelperTest.java
@@ -0,0 +1,16 @@
+package org.simple.framework.dao;
+
+import junit.framework.TestCase;
+
+public class DaoHelperTest extends TestCase {
+
+    public void testCreateInstance() throws Exception {
+
+        KeyValue keyValue = new KeyValue();
+        keyValue.setKey("hello");
+
+        DbQuery dbQuery = new DbQuery();
+        dbQuery.select().from("db_demo").where().column("id").equal("123");
+
+    }
+}
