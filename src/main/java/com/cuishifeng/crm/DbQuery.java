bce0605bfdf76981cad579305152ce9ec5d0fc32
cuishifeng <cuishifeng0207@163.com>
2022-07-12 22:18:10 +0800

    SQLConstants add

--- src/main/java/com/cuishifeng/crm/DbQuery.java
+++ src/main/java/com/cuishifeng/crm/DbQuery.java
@@ -14,13 +14,43 @@
 
 package com.cuishifeng.crm;
 
+import static com.cuishifeng.crm.model.SQLConstants.ALL_COLUMNS;
+import static com.cuishifeng.crm.model.SQLConstants.AND;
+import static com.cuishifeng.crm.model.SQLConstants.AS;
+import static com.cuishifeng.crm.model.SQLConstants.BETWEEN;
+import static com.cuishifeng.crm.model.SQLConstants.COMMA;
+import static com.cuishifeng.crm.model.SQLConstants.EQUALS;
+import static com.cuishifeng.crm.model.SQLConstants.FROM;
+import static com.cuishifeng.crm.model.SQLConstants.GREATER;
+import static com.cuishifeng.crm.model.SQLConstants.GREATER_EQUALS;
+import static com.cuishifeng.crm.model.SQLConstants.IN;
+import static com.cuishifeng.crm.model.SQLConstants.INNER;
+import static com.cuishifeng.crm.model.SQLConstants.IS_NOT_NULL;
+import static com.cuishifeng.crm.model.SQLConstants.IS_NULL;
+import static com.cuishifeng.crm.model.SQLConstants.JOIN;
+import static com.cuishifeng.crm.model.SQLConstants.LEFT;
+import static com.cuishifeng.crm.model.SQLConstants.LEFT_PARENTHESIS;
+import static com.cuishifeng.crm.model.SQLConstants.LESS;
+import static com.cuishifeng.crm.model.SQLConstants.LESS_EQUALS;
+import static com.cuishifeng.crm.model.SQLConstants.LIMIT;
+import static com.cuishifeng.crm.model.SQLConstants.NOT_IN;
+import static com.cuishifeng.crm.model.SQLConstants.NO_EQUALS;
+import static com.cuishifeng.crm.model.SQLConstants.ON;
+import static com.cuishifeng.crm.model.SQLConstants.OUTER;
+import static com.cuishifeng.crm.model.SQLConstants.QUESTION_MARK;
+import static com.cuishifeng.crm.model.SQLConstants.QUESTION_MARK2;
+import static com.cuishifeng.crm.model.SQLConstants.RIGHT;
+import static com.cuishifeng.crm.model.SQLConstants.RIGHT_PARENTHESIS;
+import static com.cuishifeng.crm.model.SQLConstants.SELECT;
+import static com.cuishifeng.crm.model.SQLConstants.SPACE;
+import static com.cuishifeng.crm.model.SQLConstants.WHERE;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 
 import com.cuishifeng.crm.util.StringChecker;
-
 import com.google.common.base.Strings;
 
 /**
@@ -41,20 +71,20 @@ public class DbQuery {
     private StringBuilder sql = new StringBuilder();
 
     public DbQuery select() throws Exception {
-        return select("*");
+        return select(ALL_COLUMNS);
     }
 
     public DbQuery select(String columns) throws Exception {
-        String[] colAry = columns.split(",");
+        String[] colAry = columns.split(COMMA);
         for (int i = 0; i < colAry.length; i++) {
             StringChecker.checkColumn(colAry[i]);
         }
 
-        sql.insert(0, " SELECT ");
+        sql.insert(0, SELECT);
         if (Strings.isNullOrEmpty(columns)) {
-            sql.append(" * ");
+            sql.append(ALL_COLUMNS);
         } else {
-            sql.append(columns).append(' ');
+            sql.append(columns).append(SPACE);
         }
 
         return this;
@@ -64,7 +94,7 @@ public class DbQuery {
         //表名合法性检查
         StringChecker.checkTableName(tableName);
 
-        sql.append(" FROM ").append(tableName);
+        sql.append(FROM).append(tableName);
 
         return this;
     }
@@ -74,32 +104,32 @@ public class DbQuery {
             //as合法性检查
             StringChecker.checkTableName(as);
 
-            sql.append(" AS ").append(as);
+            sql.append(AS).append(as);
         }
 
         return this;
     }
 
     public DbQuery leftJoin(String tableName) throws Exception {
-        return join("LEFT", tableName);
+        return join(LEFT, tableName);
     }
 
     public DbQuery rightJoin(String tableName) throws Exception {
-        return join("RIGHT", tableName);
+        return join(RIGHT, tableName);
     }
 
     public DbQuery innerJoin(String tableName) throws Exception {
-        return join("INNER", tableName);
+        return join(INNER, tableName);
     }
 
     public DbQuery fullJoin(String tableName) throws Exception {
-        return join("OUTER", tableName);
+        return join(OUTER, tableName);
     }
 
     private DbQuery join(String type, String tableName) throws Exception {
         StringChecker.checkTableName(tableName);
 
-        sql.append(' ').append(type).append(" JOIN ").append(tableName);
+        sql.append(SPACE).append(type).append(JOIN).append(tableName);
         return this;
     }
 
@@ -107,24 +137,24 @@ public class DbQuery {
         StringChecker.checkColumn(onColumn1);
         StringChecker.checkColumn(onColumn2);
 
-        sql.append(" ON ").append(onColumn1).append(" = ").append(onColumn2);
+        sql.append(ON).append(onColumn1).append(EQUALS).append(onColumn2);
         return this;
     }
 
     public DbQuery between(Object param1, Object param2) {
-        sql.append(" BETWEEN ? AND ? ");
+        sql.append(BETWEEN).append(QUESTION_MARK).append(AND).append(QUESTION_MARK);
         values.add(param1);
         values.add(param2);
         return this;
     }
 
     public DbQuery where() {
-        sql.append(" WHERE ");
+        sql.append(WHERE);
         return this;
     }
 
     public DbQuery limit(int offset, int rows) {
-        sql.append(" LIMIT ?,? ");
+        sql.append(LIMIT).append(QUESTION_MARK).append(COMMA).append(QUESTION_MARK);
         values.add(offset);
         values.add(rows);
         return this;
@@ -132,55 +162,54 @@ public class DbQuery {
 
     public DbQuery column(String column) throws Exception {
         StringChecker.checkColumn(column);
-
-        sql.append(" ");
+        sql.append(SPACE);
         sql.append(column);
-        sql.append(" ");
+        sql.append(SPACE);
         return this;
     }
 
     public DbQuery equal(Object value) {
-        sql.append(" =? ");
+        sql.append(EQUALS).append(QUESTION_MARK);
         values.add(value);
         return this;
     }
 
     public DbQuery isNull() {
-        sql.append(" is null ");
+        sql.append(IS_NULL);
         return this;
     }
 
     public DbQuery isNotNull() {
-        sql.append(" is not null ");
+        sql.append(IS_NOT_NULL);
         return this;
     }
 
     public DbQuery notEqual(Object value) {
-        sql.append(" !=? ");
+        sql.append(NO_EQUALS).append(QUESTION_MARK);
         values.add(value);
         return this;
     }
 
     public DbQuery greater(Object value) {
-        sql.append(" >? ");
+        sql.append(GREATER).append(QUESTION_MARK);
         values.add(value);
         return this;
     }
 
     public DbQuery greaterOrEqual(Object value) {
-        sql.append(" >=? ");
+        sql.append(GREATER_EQUALS).append(QUESTION_MARK);
         values.add(value);
         return this;
     }
 
     public DbQuery less(Object value) {
-        sql.append(" <? ");
+        sql.append(LESS).append(QUESTION_MARK);
         values.add(value);
         return this;
     }
 
     public DbQuery lessOrEqual(Object value) {
-        sql.append(" <=? ");
+        sql.append(LESS_EQUALS).equals(QUESTION_MARK);
         values.add(value);
         return this;
     }
@@ -189,20 +218,20 @@ public class DbQuery {
         // invalid case, create a null pointer exception.
 
         if (value.length == 1) {
-            sql.append(" =? ");
+            sql.append(EQUALS).append(QUESTION_MARK);
             values.add(value[0]);
             return this;
         }
 
-        sql.append(" IN (");
+        sql.append(IN).append(LEFT_PARENTHESIS);
         for (int i = 0; i < value.length; i++) {
             if (i != 0) {
-                sql.append(',');
+                sql.append(COMMA);
             }
-            sql.append('?');
+            sql.append(QUESTION_MARK2);
             values.add(value[i]);
         }
-        sql.append(')');
+        sql.append(RIGHT_PARENTHESIS);
         return this;
     }
 
@@ -210,59 +239,57 @@ public class DbQuery {
         // empty param is invalid by logic, just let the invoker get an invalid sql.
 
         if (value.length == 1) {
-            sql.append(" =? ");
+            sql.append(EQUALS).equals(QUESTION_MARK);
             values.add(value[0]);
             return this;
         }
-
-        sql.append(" NOT IN (");
+        sql.append(NOT_IN).append(LEFT_PARENTHESIS);
         for (int i = 0; i < value.length; i++) {
             if (i != 0) {
-                sql.append(',');
+                sql.append(COMMA);
             }
-            sql.append('?');
+            sql.append(QUESTION_MARK2);
             values.add(value[i]);
         }
-        sql.append(')');
+        sql.append(RIGHT_PARENTHESIS);
         return this;
     }
 
     public <T> DbQuery inCollection(Collection<T> value) {
         // empty param is invalid by logic, just let the invoker get an invalid sql.
-
         Iterator<T> iterator = value.iterator();
         if (value.size() == 1) {
-            sql.append(" =? ");
+            sql.append(EQUALS).append(QUESTION_MARK);
             values.add(iterator.next());
             return this;
         }
-        sql.append(" IN (");
+        sql.append(IN).append(LEFT_PARENTHESIS);
         for (int i = 0; i < value.size(); i++) {
             if (i != 0) {
-                sql.append(',');
+                sql.append(COMMA);
             }
-            sql.append('?');
+            sql.append(QUESTION_MARK2);
             values.add(iterator.next());
         }
-        sql.append(')');
+        sql.append(RIGHT_PARENTHESIS);
         return this;
     }
 
     public <T> DbQuery inList(List<T> value) {
         if (value.size() == 1) {
-            sql.append(" =? ");
+            sql.append(EQUALS).append(QUESTION_MARK);
             values.add(value.get(0));
             return this;
         }
-        sql.append(" IN (");
+        sql.append(IN).append(LEFT_PARENTHESIS);
         for (int i = 0; i < value.size(); i++) {
             if (i != 0) {
-                sql.append(',');
+                sql.append(COMMA);
             }
-            sql.append('?');
+            sql.append(QUESTION_MARK2);
             values.add(value.get(i));
         }
-        sql.append(')');
+        sql.append(RIGHT_PARENTHESIS);
         return this;
     }
 
@@ -281,7 +308,7 @@ public class DbQuery {
             values.add(value.get(i));
         }
 
-        sql.append(')');
+        sql.append(RIGHT_PARENTHESIS);
         return this;
     }
 
---
+++ src/main/java/com/cuishifeng/crm/model/SQLConstants.java
@@ -0,0 +1,65 @@
+/*
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.cuishifeng.crm.model;
+
+/**
+ * @author cuishifeng <cuishifeng@kuaishou.com>
+ * Created on 2022-07-12
+ */
+public class SQLConstants {
+
+    public static final String ALL_COLUMNS = " * ";
+    public static final String SELECT = " SELECT ";
+    public static final String FROM = " FROM ";
+    public static final String WHERE = " WHERE ";
+    public static final String AS = " AS ";
+    public static final String LEFT = " LEFT ";
+    public static final String RIGHT = " RIGHT ";
+    public static final String INNER = " INNER ";
+    public static final String OUTER = " OUTER ";
+    public static final String JOIN = " JOIN ";
+    public static final String BETWEEN = " BETWEEN ";
+    public static final String AND = " AND ";
+    public static final String FOR_UPDATE = " FOR UPDATE ";
+    public static final String GROUP_BY = " GROUP BY ";
+    public static final String UPDATE = " UPDATE ";
+    public static final String DISTINCT = " DISTINCT ";
+    public static final String COUNT = " COUNT ";
+
+    public static final String LIKE = " LIKE ";
+    public static final String LIMIT = " LIMIT ";
+    public static final String EQUALS = " = ";
+    public static final String NO_EQUALS = " != ";
+    public static final String ESPER = " & ";
+    public static final String QUESTION_MARK = " ? ";
+    public static final String QUESTION_MARK2 = " '?' ";
+
+    public static final String IS_NULL = " is null ";
+    public static final String IS_NOT_NULL = " is not null ";
+    public static final String IN = " IN ";
+    public static final String NOT_IN = " NOT IN ";
+    public static final String LEFT_PARENTHESIS = " ( ";
+    public static final String RIGHT_PARENTHESIS = " ) ";
+    public static final String COMMA = ",";
+    public static final String ON = " ON ";
+    public static final String SPACE = " ";
+    public static final String GREATER = " > ";
+    public static final String GREATER_EQUALS = " >= ";
+    public static final String LESS = " < ";
+    public static final String LESS_EQUALS = " <= ";
+
+
+
+}
